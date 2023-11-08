b921e03a99dd201642586b12c52a37ef0219ed51
Patrick Baumgartner <patrick.baumgartner@42talents.com>
2023-11-08 19:42:08 +0100

    Updated slides for W-JAX 2023

--- examples/spring-petclinic-crac/restore.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker run --rm -p 8080:8080 --name spring-petclinic-crac spring-petclinic-crac:checkpoint
--- examples/spring-petclinic-crac/src/scripts/entrypoint-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 128 > /proc/sys/kernel/ns_last_pid; java -XX:CRaCCheckpointTo=/opt/crac-files -jar /opt/app/spring-petclinic-crac-3.2.0-SNAPSHOT.jar&
-sleep 5
-jcmd /opt/app/spring-petclinic-crac-3.2.0-SNAPSHOT.jar JDK.checkpoint
-sleep infinity
